dda5d96306ab4e9630102d1be7bd44df39e06b1c
kstvr32 <kstvr32@gmail.com>
2024-09-17 23:23:36 -0400

    add setup

---
+++ setup.lua
@@ -0,0 +1,40 @@
+local shell = require('shell')
+local filesystem = require("filesystem")
+local args = {...}
+local branch
+local repo
+local libs = {
+    'crc32.lua',
+    'deflate.lua',
+    'item.lua',
+    'json.lua',
+    'log.lua',
+    'nbt.lua'
+}
+
+-- BRANCH
+if #args >= 1 then
+    branch = args[1]
+else
+    branch = 'main'
+end
+
+-- REPO
+if #args >= 2 then
+    repo = args[2]
+else
+    repo = 'https://raw.githubusercontent.com/kstvr32/OpenComputerScripts/'
+end
+
+-- INSTALL LIB
+local workDir = "/home/"
+local libDir = workDir .. "lib/"
+
+print("Removing Library")
+filesystem.remove(libDir)
+filesystem.makeDirectory(libDir)
+shell.setWorkingDirectory(libDir)
+for i=1, #libs do
+    shell.execute(string.format('wget -f %s%s/%s', repo, branch, libs[i]))
+    print("downloaded", libs[i])
+end
