ac3ac3ccbadfb64159256fb2a90bc733e3d7621e
kstvr32 <kstvr32@gmail.com>
2024-09-17 23:59:26 -0400

    properly write tag

--- test.lua
+++ test.lua
@@ -4,13 +4,31 @@ local itemLib = require("item")
 
 local chestStacks = component.transposer.getAllStacks(sides.up)
 
-for item in chestStacks directory
+function dump(o)
+    if type(o) == 'table' then
+       local s = '{ '
+       for k,v in pairs(o) do
+          if type(k) ~= 'number' then k = '"'..k..'"' end
+          s = s .. '['..k..'] = ' .. dump(v) .. ','
+       end
+       return s .. '} '
+    else
+       return tostring(o)
+    end
+ end
+
+for item in chestStacks do
     if item.name ~= nil then
         print("label", item.label)
         print("stackSize", item.size)
         print("damage", item.damage)
+        itemLib.parseStack(item)
         if item.hasTag then
-            print(itemlib.parseTag(item.tag))
+            local tag = item.tag
+            print(dump(tag))
+            for k, tag in ipairs(tag) do
+                print(k, tag)
+            end
         end
         print("------------")
     end
